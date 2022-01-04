da431df4358ae7c677a56e70a32150ca2757fd51
Mihail <skroba.misha@gmail.com>
2022-01-04 08:04:52 +0300

    Test commit

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// alert('Привет');
+
+// confirm ('Водку будкшь ?');
+
+/* let a = prompt("Тест","");
+console.log(a); */
+
+/* const answers = [];
+
+answers[0] = prompt("Введите имя","");
+answers[1] = prompt("Введите фамилию","");
+answers[2] = prompt("Введите возраст","");
+
+document.write(answers); */
+
+/* console.log(typeof(null)); */
+
+// Интерполяция
+const param = 'pavel';
+
+console.log(`https://someurl.com/${param}/1`)
